7735823be23173e11363d6ea3ede704dc829fc45
vnoook <noook@yandex.ru>
2021-09-22 13:59:22 +0700

    #5

--- first_oop1.py
+++ first_oop1.py
@@ -1,16 +1,17 @@
+
 class MyFirstClass():
     x1 = 111
-    print('__common__')
-    print(f'{x1 = }')
-    print()
+    # print('__common__')
+    # print(f'{x1 = }')
+    # print()
     def __init__(self):
-        x2 = 222
         x1 = 444
-        print('__init__')
-        print(x2)
+        x2 = 222
+        # print('__init__')
+        # print(x2)
         # print(self.x2)
         # print(MyFirstClass.x2)
-        print()
+        # print()
 
 print(f'{MyFirstClass.x1 = }')
 
