f42e3931f7f9edfe4d1622935e337216ddc0a593
vnoook <noook@yandex.ru>
2021-09-23 15:04:36 +0700

    $7

--- first_oop1.py
+++ first_oop1.py
@@ -4,14 +4,28 @@ class MyFirstClass():
         x1 = 444
         x2 = 222
 
+print()
 print(f'{MyFirstClass.x1 = }')
-# MyFirstClass.x1 = 333
-# print(f'{MyFirstClass.x1 = }')
 
 a = MyFirstClass()
 print(f'{a.x1 = }')
+print(f'{a.__getattribute__("x1") = }')
+a.x1 = 123
+print(f'{a.x1 = }')
+print()
+
+MyFirstClass.x1 = 333
+print(f'{MyFirstClass.x1 = }')
+print()
+
+print(f'{a.x1 = }')
+print(f'{a.__getattribute__("x1") = }')
+print()
+
+# print(f'{a.x1 = }')
 # a.x1 = 223344
 # print(f'{a.x1 = }')
 
-
+for val in list(globals()):
+    print(f'globals()[{val}] = {type(globals()[val])}, {globals()[val]}')
 
