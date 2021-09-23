fa9da70f810e41ae2e821d250d05fb7e6d7a336e
vnoook <noook@yandex.ru>
2021-09-23 16:21:38 +0700

    $8

--- first_oop1.py
+++ first_oop1.py
@@ -1,31 +1,48 @@
-class MyFirstClass():
-    x1 = 111
+class MyFirstClass:
+    # статический атрибут
+    x1 = 000
+
     def __init__(self):
-        x1 = 444
-        x2 = 222
+        # динамические атрибуты
+        self.x1 = 111
+        self.x2 = 222
+        self.x3 = 333
+
+    def get_obj_name(self):
+        for glob_name, glob_val in globals().items():
+            if glob_val is self:
+                return glob_name
+
 
 print()
+print(f'обращаюсь к статическому атрибуту x1 класса {MyFirstClass.__name__}')
 print(f'{MyFirstClass.x1 = }')
+print()
 
 a = MyFirstClass()
-print(f'{a.x1 = }')
-print(f'{a.__getattribute__("x1") = }')
-a.x1 = 123
-print(f'{a.x1 = }')
+print(f'создаю объект {a.get_obj_name()} и обращаюсь к его атрибуту двумя способами')
+print(f'{a.x1 = }  ...  {a.__getattribute__("x1") = }')
 print()
 
+print(f'меняю значение атрибута объекта {a.get_obj_name()} и обращаюсь к нему')
+a.x1 = 123
+print(dir(a.x1))
+
+print(f'{a.x1 = }  ...  {a.__getattribute__("x1") = }')
+print()
+
+print(f'меняю в классе {MyFirstClass.__name__} атрибут и обращаюсь к нему')
 MyFirstClass.x1 = 333
 print(f'{MyFirstClass.x1 = }')
 print()
 
-print(f'{a.x1 = }')
-print(f'{a.__getattribute__("x1") = }')
+# обращаюсь к атрибуту объекта
+print(f'{a.x1 = }  ...  {a.__getattribute__("x1") = }')
 print()
 
 # print(f'{a.x1 = }')
 # a.x1 = 223344
 # print(f'{a.x1 = }')
 
-for val in list(globals()):
-    print(f'globals()[{val}] = {type(globals()[val])}, {globals()[val]}')
-
+# for val in list(globals()):
+#     print(f' globals()[{val}] = {type(globals()[val])}, {globals()[val]}')
