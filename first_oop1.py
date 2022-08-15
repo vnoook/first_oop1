5d80a39120d6c814f99a2b7f54b2df4e4a5748ba
vnoook <noook@yandex.ru>
2022-08-15 21:49:55 +0700

    #15

--- first_oop1.py
+++ first_oop1.py
@@ -4,6 +4,7 @@ class MyFirstClass:
     # статический атрибут, свойство класса
     x1 = 000
 
+    # конструктор
     def __init__(self):
         # динамические атрибуты, свойства экземпляра
         self.x1 = 111
@@ -12,6 +13,7 @@ class MyFirstClass:
         self._y1 = 'protect1'  #  защищённый атрибут
         self.__y2 = 'privat1'  #  приватный атрибут
 
+    # метод получения имени
     def get_obj_name(self):
         for glob_name, glob_val in globals().items():
             if glob_val is self:
@@ -19,13 +21,22 @@ class MyFirstClass:
 
 
 if __name__ == '__main__':
+    # эксперименты с классом
     print(MyFirstClass.__dict__)
     print(MyFirstClass().__dict__)
     print()
+    print(MyFirstClass.get_obj_name(MyFirstClass))
+    print()
+    print(MyFirstClass.__mro__)
+    print()
+    print(id(MyFirstClass))
+    print(id(MyFirstClass()))
+    print()
     print(f'обращаюсь к статическому атрибуту "x1" класса "{MyFirstClass.__name__}"')
     print(f'{MyFirstClass.x1 = }')
-    print()
+    print('_'*150)
 
+    # эксперименты с экземпляром
     a = MyFirstClass()
     print(f'создаю объект "{a.get_obj_name()}" и обращаюсь к его атрибуту "x1" двумя способами')
     print(f'{a.x1 = }  ...  {a.__getattribute__("x1") = }')
@@ -38,7 +49,7 @@ if __name__ == '__main__':
 
     print(f'меняю в классе "{MyFirstClass.__name__}" атрибут и обращаюсь к нему')
     MyFirstClass.x1 = 333
-    print(f'{MyFirstClass.x1 = }')
+    print(f'{MyFirstClass.x1 = } ... {MyFirstClass.__getattribute__(MyFirstClass, "x1") = }')
     print()
 
     print(f'обращаюсь к атрибуту объекта')
@@ -71,7 +82,4 @@ if __name__ == '__main__':
     print()
     print(a.__dict__)
     print(a.__dir__())
-
-    print()
-
-    aa = 0
+    print(dir(a))
